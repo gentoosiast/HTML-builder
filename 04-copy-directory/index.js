08af22ed3c2d29d5031759fc569758cd9b446a7b
Sergey Koksharov <gentoosiastic@gmail.com>
2022-10-31 00:38:37 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
 
+const copyDir = async (srcDir, dstDir) => {
+  try {
+    await fsPromises.rm(dstDir, { force: true, recursive: true });
+    await fsPromises.mkdir(dstDir, { recursive: true });
+    const dirents = await fsPromises.readdir(srcDir, { withFileTypes: true });
+
+    for (const entry of dirents) {
+      const from = path.resolve(srcDir, entry.name);
+      const to = path.resolve(dstDir, entry.name);
+
+      if (entry.isDirectory()) {
+        await copyDir(from, to);
+      } else if (entry.isFile()) {
+        await fsPromises.copyFile(from, to);
+      }
+    }
+  } catch (error) {
+    console.error('There was an error:', error.message);
+  }
+};
+
+const srcDir = path.resolve(__dirname, 'files');
+const dstDir = path.resolve(__dirname, 'files-copy');
+copyDir(srcDir, dstDir);
